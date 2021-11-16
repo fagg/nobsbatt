64cb68c5aae2d24c1dc2492f51583256a7ead831
Ashton Fagg <ashton@fagg.id.au>
2021-11-16 07:54:53 -0500

    Changes for -current

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 #define TEXT_COLOR { 0, 0, 0, 65535 }
 #define X_GEOM "80x80-1-80"
 #define WINDOW_NAME "NoBSBatt"
-#define FONT_NAME "Spleen 16x32"
+#define FONT_NAME "Spleen:pixelsize=16"
 
 /* Poll time */
 #define POLL_SECONDS 3
